6bc77e9d652f992cd4553e95c58289ef3e15cea4
tyh <109740354+tyh-1@users.noreply.github.com>
2025-11-18 16:07:18 +0800

    fix: 移除雲端環境的本地 CSV 儲存

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ else:
     from spotify_log import spotify_refresh_tok_flow
     df = spotify_refresh_tok_flow.fetch_recently_played(my_config['refresh_token'])
 
-
-file_nm = "data/1118.csv"
-if os.path.exists(file_nm): raise FileExistsError
-df.to_csv(file_nm)
+# 如果在本地，就順便存 csv. 提供 debug 素材
+if not my_config["is_cloud"]:
+    file_nm = "data/1118.csv"
+    if os.path.exists(file_nm): raise FileExistsError
+    df.to_csv(file_nm)
 
 # 更新到 db
 sqlite_utils.create_tables_if_not_exists()
